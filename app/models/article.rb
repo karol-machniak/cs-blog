60517cc93e8bd7d079adfb9fb29fb621ac45e10b
karol-machniak <karol.machniak87@gmail.com>
2019-01-05 15:03:50 +0100

    Add Comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    has_many :comments
   validates :title, :text, presence: true
   validates :title, length: { minimum: 5, maximum: 40 }
   validates :title, uniqueness: true, if: :title_changed?
