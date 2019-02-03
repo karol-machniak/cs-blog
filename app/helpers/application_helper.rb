82853dc40caf8cb77bc5c0f11c3e8d36bf40508c
karol-machniak <karol.machniak87@gmail.com>
2019-02-03 10:05:11 +0100

    add bootstrap & replacing erb with haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bootstrap_flash_color(flash_type)
+    case flash_type.to_sym
+    when :error then 'danger'
+    when :alert then 'warning'
+    when :notice then 'info'
+    when :success then 'succes'
+    else "secondary"
+    end
+  end 
 end
