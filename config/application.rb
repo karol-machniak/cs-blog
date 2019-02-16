897e34769cb7e98770ea9f193551b85eb600e83d
karol-machniak <karol.machniak87@gmail.com>
2019-02-16 16:24:32 +0100

    use activeJob for mailers

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
+ 
     # the framework and any gems in your application.
+    config.active_job.queue_adapter = :sidekiq
   end
 end
