92ede61a2f1cce564db81995e9bd9ecef8eb2d93
karol-machniak <karol.machniak87@gmail.com>
2019-02-17 11:56:36 +0100

    test fix 2

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
   should validate_presence_of(:title)
-  should validate_lenght_of(:title).is_at_least(5).is_at_most(40)
+  should validate_length_of(:title).is_at_least(5).is_at_most(40)
   should validate_presence_of(:text)
   should validate_acceptance_of(:terms_of_service)
  
